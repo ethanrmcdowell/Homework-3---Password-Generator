71adddcdea645fee206f8547495f80167c31706b
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2020-09-27 14:48:13 -0400

    Updated some notes, rearranged JS code to be easier to read, switched site theme in CSS so it isn't quite as blinding on the eyes

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-  document.getElementById("password").textContent = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 // CREATED VARIABLE TO CONTAIN PASSWORD ARRAY AS WELL AS VARIABLES FOR EACH CHARACTER WE COULD BE USING TO CREATE THE PASSWORD
-
 var password = [];
-
 var capLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ",", "-", "=", "+", "[", "]", "{", "}", ";", ":", "'", "/", "?", "<", ">", ".", "|"];
 var pNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var generateBtn = document.querySelector("#generate");
+
+// THE PASSWORD THAT IS RETURNED FROM THE GENERATEPASSWORD FUNCTION IS THEN PULLED INTO THIS FUNCTION WHICH CHANGES THE TEXT AREA IN THE HTML TO SHOW THE GENERATED PASSWORD
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  document.getElementById("password").innerHTML = password;
+}
+
+// EVENT LISTENER, WHEN THE GENERATE PASSWORD BUTTON IS CLICKED IT WILL RUN THE WRITEPASSWORD FUNCTION
+generateBtn.addEventListener("click", writePassword);
 
 // USER PROMPTS TO DECIDE HOW LONG PASSWORD SHOULD BE AND WHAT TYPE OF CHARACTERS SHOULD BE USED, IF INCORRECT INPUT IS GIVEN WILL STOP THE PROCESS
-
 function generatePassword() {
   characterAmount = prompt("Please choose how many characters you would like your password to be, between 8 and 128.");
   if (!characterAmount) {
@@ -40,21 +32,22 @@ function generatePassword() {
   }
 
   // THESE IF STATEMENTS TAKE THE USER INPUT THROUGH PROMPTS AND COMBINE THE ARRAYS CONTAINING CHARACTERS THE USER WISHES TO USE
-
   if (passNumber) {
     selection = pNumbers;
-  } if (passSpecial) {
+  }
+  if (passSpecial) {
     selection = selection.concat(specChar);
-  } if (passCapital) {
+  }
+  if (passCapital) {
     selection = selection.concat(capLetter);
-  } if (passLower) {
+  }
+  if (passLower) {
     selection = selection.concat(lowLetter);
   }
 
   // FOR LOOP WHICH WILL RANDOMLY ASSIGN A CHARACTER IN THE FINAL CHARACTER ARRAY - WILL CONTINUE TO ADD CHARACTERS AT RANDOM UNTIL IT HAS REACHED
   // THE AMOUNT OF CHARACTERS THE USER REQUESTED - WILL THEN TAKE THE NEW PASSWORD ARRAY, TURN IT INTO A STRING, AND PUSH IT TO BE USED.
-
-  for(var i = 0; i < characterAmount; i++) {
+  for (var i = 0; i < characterAmount; i++) {
     var j = selection[Math.floor(Math.random() * selection.length)];
     password.push(j);
   }
